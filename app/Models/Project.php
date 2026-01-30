9afada35a87353b66a7bd10ff358021d91c6e68b
antaGB <antapermana1156@gmail.com>
2026-01-30 17:13:31 +0800

    feat: add model relation and empty resource controller for all table

---
+++ app/Models/Project.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Project extends Model
+{
+    protected $fillable = ['name', 'description', 'status'];
+
+    public function tasks(): HasMany {
+        return $this->hasMany(Task::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -44,5 +46,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Relation to Roles (Pivot: role_user)
+    public function roles(): BelongsToMany {
+        return $this->belongsToMany(Role::class);
+    }
+
+    // Relation to Tasks for this user
+    public function tasks(): HasMany {
+        return $this->hasMany(Task::class, 'assigned_to');
     }
 }
