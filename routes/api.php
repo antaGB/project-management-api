941b38b073b2627deecfcdf7f337d31a8d9d10c8
antaGB <antapermana1156@gmail.com>
2026-01-30 19:35:20 +0800

    feat: register all endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,30 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\PermissionController;
 
 // Public Route
 Route::post('/login', [AuthController::class, 'login']);
 
 // Protected Routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user-profile', function (Request $request) {
-        return $request->user();
-    });
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('tasks', TaskController::class);
+    Route::apiResource('roles', RoleController::class);
+    Route::apiResource('users', UserController::class)->middleware('permission:manage-users');
+    Route::apiResource('permissions', PermissionController::class)->middleware('permission:manage-permissions');
+    
+    // Assign Permission to Role
+    Route::post('roles/{role}/permissions', [RoleController::class, 'givePermission']);
+    
+    // Assign Role to User
+    Route::post('users/{user}/assign-role', [UserController::class, 'assignRole']);
+
+    
     
 });
