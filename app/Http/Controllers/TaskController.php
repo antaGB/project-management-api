5604fe8839f51865bf90a346569d0dffa050a530
antaGB <antapermana1156@gmail.com>
2026-01-30 19:29:05 +0800

    feat: adding crud endpoint and access

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
+    use ApiResponse;
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $tasks = Task::with(['assignee', 'project'])
+            ->when($request->project_id, function($query) use ($request) {
+                $query->where('project_id', $request->project_id);
+            })
+            ->get();
+
+        return $this->success(TaskResource::collection($tasks), 'Daftar tugas berhasil diambil');
     }
 
     /**
@@ -28,7 +38,8 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $task = Task::create($request->validated());
+        return $this->success(new TaskResource($task), 'Tugas berhasil ditambahkan', 201);
     }
 
     /**
@@ -36,7 +47,7 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
-        //
+        return $this->success(new TaskResource($task->load(['assignee', 'project'])), 'Detail tugas ditemukan');
     }
 
     /**
@@ -52,7 +63,8 @@ class TaskController extends Controller
      */
     public function update(Request $request, Task $task)
     {
-        //
+        $task->update($request->validated());
+        return $this->success(new TaskResource($task), 'Tugas berhasil diperbarui');
     }
 
     /**
@@ -60,6 +72,7 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return $this->success(null, 'Tugas berhasil dihapus');
     }
 }
---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'         => $this->id,
+            'title'      => $this->title,
+            'priority'   => strtoupper($this->priority),
+            // Avoid null coalescing error if assignee is empty
+            'assignee'   => $this->assignee ? [
+                'id'    => $this->assignee->id,
+                'name'  => $this->assignee->name,
+            ] : null,
+            'project'    => $this->whenLoaded('project', function() {
+                return $this->project->name;
+            }),
+        ];
+    }
+}
