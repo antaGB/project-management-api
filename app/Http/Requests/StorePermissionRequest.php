2e6083c11ad0a11a66ca352c630e8a774e163e0e
antaGB <antapermana1156@gmail.com>
2026-02-05 22:39:19 +0800

    fix: fix permissions

--- app/Http/Requests/StorePermissionRequest.php
+++ app/Http/Requests/StorePermissionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StorePermissionRequest extends FormRequest
@@ -21,8 +22,14 @@ class StorePermissionRequest extends FormRequest
      */
     public function rules(): array
     {
+        $permission = $this->route('permission');
+
         return [
-            'name' => 'required|string|unique:permissions,name,' . ($this->permission?->id),
+            'name' => [
+                'required', 
+                'string', 
+                Rule::unique('permissions')->ignore($permission)
+            ],
             'description' => 'nullable|string'
         ];
     }
