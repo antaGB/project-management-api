f6073191e0b906b90b6f0fafcdf0c7f23f80c82a
antaGB <antapermana1156@gmail.com>
2026-02-06 21:07:23 +0800

    fix: use viewAny policy on index endpoint instead of view

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -12,7 +12,7 @@ class UserPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return $user->hasPermission('view-user');
     }
 
     /**
@@ -20,7 +20,7 @@ class UserPolicy
      */
     public function view(User $user, User $model): bool
     {
-        return $user->hasPermission('view-user') || $user->id === $model->id;;
+        return $user->hasPermission('view-user') || $user->id === $model->id;
     }
 
     /**
